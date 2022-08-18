c3670e0a3eac68c3a1e8245f79babcd7534b5a9c
bilzkaist <63588157+bilzkaist@users.noreply.github.com>
2022-08-18 22:26:18 +0900

    Server-Client is working...

---
+++ client.py
@@ -0,0 +1,12 @@
+import socket
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 2020))
+
+client.send(str.encode(("I am CLIENT\n")))
+
+from_server = client.recv(4096)
+
+client.close()
+
+print(from_server)
